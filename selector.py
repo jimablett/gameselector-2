ed922ba2486c6a5089be1afd6909a1d9930dd40b
Jim Ablett <jim.ablett@gmail.com>
2024-10-23 21:45:57 +0100

    Add files via upload
    1.  Fixed bug in percentage processed indicator not showing correct percentage processed
    2.   Added 'players_bad_games.txt' andplayer_totals_bad_games.txt reports which will be created at the end showing bad game stats for individual engines e.g >

--- selector.py
+++ selector.py
@@ -5,28 +5,71 @@ import chess.engine
 import sys
 import time
 import colorama
+import subprocess
+from collections import defaultdict
 
 colorama.init(autoreset=True)
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-__script_name__ = 'game-selector 2'
+__script_name__ = '    game-selector 2'
 __goal__ = 'Separate good and bad games'
-__version__ = '0.2.5_JA'
+__version__ = '0.2.6_JA'
 
 
 def print_progress(iteration, total, prefix=''):
-    percent = (iteration  / total) * 1000
-    percent *=2.5
+    percent = (iteration / total) * 100
     if total > 0:
         percent = min(percent, 100)
     sys.stdout.write(f'\r{colorama.Fore.GREEN}{prefix} : {percent:.0f}% Complete')
     sys.stdout.flush()
 
 
+def delete_output_files():
+    output_folder = 'output'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for filename in os.listdir(output_folder):
+        file_path = os.path.join(output_folder, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            print(f"Error deleting file {file_path}: {e}")
+
+
+def open_output_folder():
+    output_folder = os.path.abspath('output')
+    subprocess.Popen(['explorer.exe', output_folder])
+
+
+def create_no_bad_games_file():
+    output_folder = 'output'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    with open(os.path.join(output_folder, 'no_bad_games_found.txt'), 'w') as f:
+        f.write("no bad games were found in this pgn.")
+
+
+def merge_duplicate_entries(file_path):
+    player_stats = defaultdict(int)
+    
+    with open(file_path, 'r') as f:
+        for line in f:
+            if line.strip():
+                parts = line.split('=')
+                if len(parts) == 2:
+                    player_name, score = parts
+                    player_stats[player_name.strip()] += int(score.strip())
+    
+    with open(file_path, 'w') as f:
+        for player_name, total_score in player_stats.items():
+            f.write(f"{player_name} = {total_score}\n")
+
+
 def main():
     parser = argparse.ArgumentParser(
-        prog='%s' % __script_name__,
+        prog=__script_name__,
         description=__goal__, epilog='%(prog)s')
     parser.add_argument('--input', required=True, type=str,
                         help='Input pgn filename (required=True).')
@@ -57,26 +100,29 @@ def main():
     cnt = 0
     not_removed_games = []
 
+    delete_output_files()
+
     engine = chess.engine.SimpleEngine.popen_uci(enginefn)
     
     report_data = {
-        'wins_on_time': [],
-        'illegal_moves': [],
-        'crashed_games': [],
-        'drawn_in_winning_position': []
+        'wins_on_time': {'White': 0, 'Black': 0},
+        'illegal_moves': {'White': 0, 'Black': 0},
+        'crashed_games': {'White': 0, 'Black': 0},
+        'drawn_in_winning_position': {'White': 0, 'Black': 0}
     }
 
     with open(fn) as h:
-        total_games = sum(1 for _ in h)
+        total_games = sum(1 for line in h if 'White' in line)
         h.seek(0)
-
+        
         while True:
             game = chess.pgn.read_game(h)
-            
-            print_progress(cnt, total_games, prefix='Processing games')  
-            
+                     
             if game is None:
                 break
+            
+            print_progress(cnt, total_games, prefix='Processing games')  
+           
             cnt += 1
             
             result = game.headers['Result']
@@ -98,7 +144,7 @@ def main():
                     is_bad = True
 
                 if is_bad:
-                    fen = node.board().fen()  # pos after move
+                    fen = node.board().fen()
                     print(f' ')
                     print(f'\ngame_num: {cnt}, result: {result}, comment: {comment}')
                     print(f'fen: {fen}')
@@ -107,19 +153,7 @@ def main():
                     board = chess.Board(fen)
                     info = engine.analyse(board, chess.engine.Limit(time=movetimesec))
                     score_wpov = info['score'].white().score(mate_score=32000)
-                    score_wpov /= 100
-                    
-                    if 'wins on time' in comment:
-                        report_data['wins_on_time'].append(str(game))
-                    elif any(keyword in comment for keyword in [
-                        'Arena Adjudication. Illegal move!', 
-                        'polyglot: resign (illegal engine move', 
-                        'Forfeit due to invalid move']):
-                        report_data['illegal_moves'].append(str(game))
-                    elif 'exited unexpectedly' in comment:
-                        report_data['crashed_games'].append(str(game))
-                    elif 'but bare king} 1/2-1/2' in comment:
-                        report_data['drawn_in_winning_position'].append(str(game))
+                    score_wpov /= 100                   
 
                     if (result == '0-1' and score_wpov > -score_margin) or \
                        (result == '1/2-1/2' and score_wpov > -score_margin) or \
@@ -128,11 +162,13 @@ def main():
                         print(f'{colorama.Fore.RED}{colorama.Style.BRIGHT}will not keep this game, eval: {score_wpov} wpov{colorama.Style.RESET_ALL}')
                         print(f'\n ')
                         is_save = False
+                      
                     else:
                         print(f'{colorama.Fore.YELLOW}{colorama.Style.BRIGHT}this game will be kept, eval: {score_wpov} wpov{colorama.Style.RESET_ALL}')
                         not_removed_games.append(str(game))
                         print(f' ')
                         is_save = True
+                        
                     break
                     
             if is_save:
@@ -144,20 +180,104 @@ def main():
 
     engine.quit()
     
-    with open('kept games_(score_margin_reached).txt', 'w') as f:
+    with open('kept games_(score_margin_reached).pgn', 'w') as f:
         for game in not_removed_games:
             f.write(f'{game}\n\n')
 
-    report_file_path = os.path.join('output', 'report.txt')
-    with open(report_file_path, 'w') as report_file:
-        for category, games in report_data.items():
-            report_file.write(f"\n***********************************:\n**** {category} ****:\n***********************************:\n" + "\n".join(games) + "\n\n")
 
-    os.rename('kept games_(score_margin_reached).txt', os.path.join('output', 'kept games_(score_margin_reached).txt'))
+    os.rename('kept games_(score_margin_reached).pgn', os.path.join('output', 'kept games_(score_margin_reached).pgn'))
 
-    print_progress(total_games, total_games, prefix='Processing games')
-   
-    print('\nProcessing complete.')
+    print_progress(cnt, total_games, prefix='Processing games')  
+    
+    if os.path.exists(os.path.join('output', 'bad.pgn')):
+        with open(os.path.join('output', 'bad.pgn'), 'r') as bad_pgn_file:
+            player_totals = defaultdict(int)
+            while True:
+                bad_game = chess.pgn.read_game(bad_pgn_file)
+                if bad_game is None:
+                    break
+                    
+                white_engine_name = bad_game.headers['White']
+                black_engine_name = bad_game.headers['Black']
+                result = bad_game.headers['Result']
+            
+                for node in bad_game.mainline():
+                    comment = node.comment                
+                    if 'wins on time' in comment:
+                        report_data['wins_on_time']['White' if result == '0-1' else 'Black'] += 1
+                    elif any(keyword in comment for keyword in [
+                        'Arena Adjudication. Illegal move!', 
+                        'polyglot: resign (illegal engine move', 
+                        'Forfeit due to invalid move']):
+                        report_data['illegal_moves']['White' if result == '0-1' else 'Black'] += 1
+                    elif 'exited unexpectedly' in comment:
+                        report_data['crashed_games']['White' if result == '0-1' else 'Black'] += 1
+                    elif 'but bare king} 1/2-1/2' in comment:
+                        report_data['drawn_in_winning_position']['White' if result == '1/2-1/2' else 'Black'] += 1
+                
+                report_lines = []
+                if result == '0-1' or result == '1-0' or result == '1/2-1/2' :
+                    report_lines.append(f"White: {white_engine_name}")
+                    report_lines.append(f"Black: {black_engine_name}")
+                if report_data['wins_on_time']['White'] > 0:
+                    report_lines.append(f"game lost on time [White]")
+                    report_data['wins_on_time']['White'] = 0
+                    player_totals[f"{white_engine_name}_total_bad_games"] += 1
+                
+                elif report_data['wins_on_time']['Black'] > 0:
+                    report_lines.append(f"game lost on time [Black]")
+                    report_data['wins_on_time']['Black'] = 0
+                    player_totals[f"{black_engine_name}_total_bad_games"] += 1
+               
+                elif report_data['crashed_games']['White'] > 0:
+                    report_lines.append(f"game lost by crash [White]")
+                    report_data['crashed_games']['White'] = 0            
+                    player_totals[f"{white_engine_name}_total_bad_games"] += 1
+                
+                
+                elif report_data['crashed_games']['Black'] > 0:
+                    report_lines.append(f"game lost by crash [Black]")
+                    report_data['crashed_games']['Black'] = 0            
+                    player_totals[f"{black_engine_name}_total_bad_games"] += 1
+                
+                
+                elif report_data['illegal_moves']['White'] > 0:
+                    report_lines.append(f"game lost by illegal move [White]")                             
+                    report_data['illegal_moves']['White'] = 0 
+                    player_totals[f"{white_engine_name}_total_bad_games"] += 1
+                
+                elif report_data['illegal_moves']['Black'] > 0:
+                    report_lines.append(f"game lost by illegal move [Black]")
+                    report_data['illegal_moves']['Black'] = 0
+                    player_totals[f"{black_engine_name}_total_bad_games"] += 1
+                        
+                elif report_data['drawn_in_winning_position']['White'] > 0:
+                    report_lines.append(f"game drawn in winning position [White]")
+                    report_data['drawn_in_winning_position']['White'] = 0
+                    player_totals[f"{white_engine_name}_total_bad_games"] += 1
+                            
+                elif report_data['drawn_in_winning_position']['Black'] > 0:
+                    report_lines.append(f"game drawn in winning position [Black]")                  
+                    report_data['drawn_in_winning_position']['Black'] = 0
+                    player_totals[f"{black_engine_name}_total_bad_games"] += 1
+                    
+                    
+                with open(os.path.join('output', 'players_bad_games.txt'), 'a') as players_file:
+                    players_file.write("\n".join(report_lines) + "\n\n")
+
+            with open(os.path.join('output', 'player_totals_bad_games.txt'), 'w') as totals_file:
+                for player, total in player_totals.items():
+                    totals_file.write(f"\n{player} = {total}\n")
+
+    print_progress(total_games, total_games, prefix='Processing games')  
+    
+    if cnt == total_games and not os.path.exists(os.path.join('output', 'bad.pgn')):
+        create_no_bad_games_file()
+
+    open_output_folder()
+
+    print(f'\n ')
+
 
 if __name__ == '__main__':
     main()
